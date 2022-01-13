1210bfa508f5f38ecc450bf827220b65f42cca2b
Liza <elizaveta1207@gmail.com>
2022-01-13 15:18:21 +0300

    feat: add ts for index

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,13 @@ import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import { rootReducer } from './services/reducers';
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-    : compose;
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const enhancer = composeEnhancers(applyMiddleware(thunk));
 

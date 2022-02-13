02db00092968e46a7d0a071adb1b6a4c65304c3f
Elizaveta-1207 <elizaveta1207@gmail.com>
2022-02-13 17:06:57 +0300

    feat: add route for deploy

--- src/index.tsx
+++ src/index.tsx
@@ -28,7 +28,7 @@ export const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <App />
       </Provider>
